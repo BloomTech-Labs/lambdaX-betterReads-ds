344884e8ff2e3e715be84a890aaa87a993db4888
jazzathoth <iv.been.joe@gmail.com>
2020-03-06 16:11:59 -0500

    setting up django/graphene

---
+++ gql_betterr/gql_recommends/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GqlRecommendsConfig(AppConfig):
+    name = 'gql_recommends'
---
+++ gql_betterr/gql_recommends/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class User(models.Model):
+    user_id = models.TextField(blank=True)
+
+
+
+
+# Create your models here.
